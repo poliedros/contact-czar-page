91434f0ab3884fea13dcc66ceb9ea6e760f9cf75
Carlos <carloshlzansavio@gmail.com>
2022-08-08 14:58:06 +0100

    feat: change cors to czar.dev

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: 'localhost:3000, czar.dev',
   });
   await app.listen(process.env.PORT || 3000);
 }
