59db95a43a487507b87697f974859043d373da27
Carlos <carloshlzansavio@gmail.com>
2022-08-08 14:26:18 +0100

    feat: add envs

--- src/page-email/page-email.module.ts
+++ src/page-email/page-email.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { PageEmailController } from './page-email.controller';
 import { EmailModule } from '@czarpoliedros/email';
 import { PageEmailService } from './page-email.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [PageEmailController],
   imports: [
+    ConfigModule.forRoot(),
     EmailModule.forRoot({
       email: process.env.EMAIL,
       password: process.env.PASSWORD,
