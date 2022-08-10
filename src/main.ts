04e781e0037e1dbc4784f5160fda107b965f74b9
Carlos <carloshlzansavio@gmail.com>
2022-08-10 22:26:49 +0100

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'localhost:3000, czar.dev',
+    origin: '*.czar.dev',
   });
   await app.listen(process.env.PORT || 3000);
 }
